77da42e2d13bf845a333cf41963e174eee3e9c58
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-05-26 15:01:34 +0000

    worked on the authentication

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,19 @@ import { Routes, Route } from "react-router-dom";
 import Video from "./pages/video/Video";
 import Pdf from "./pages/pdf/Pdf";
 import Audio from "./pages/audio/Audio";
+import { AuthContext } from "./context/AuthContext/AuthContext";
+import { useContext } from "react";
 
 const App = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div className="app">
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="/image" element={<Image />} />
-        <Route path="/video" element={<Video />} />
-        <Route path="/audio" element={<Audio />} />
-        <Route path="/pdf" element={<Pdf />} />
+        <Route index element={user ? <Home /> : <Login />} />
+        <Route path="/image" element={user ? <Image /> : <Login />} />
+        <Route path="/video" element={user ? <Video /> : <Login />} />
+        <Route path="/audio" element={user ? <Audio /> : <Login />} />
+        <Route path="/pdf" element={user ? <Pdf /> : <Login />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
